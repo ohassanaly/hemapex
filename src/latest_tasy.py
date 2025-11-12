fdf597568d4df20a50fab30b7aa8a2946ca20f6d
ohassanaly <olivierhass@gmail.com>
2025-11-12 19:46:04 +0100

    clean text extraction from Tasy scrapped EMR

---
+++ src/latest_tasy.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from config import tasy_tratamento_columns
+
+
+def latest_tasy(
+    input_path: str, output_path: str, text_columns=tasy_tratamento_columns
+) -> None:
+    """
+    Input : the scrapping result of registro.tasy
+    retrieve the latest consultation for each patient, with the "text" column containing the relevant text
+    export the result as a csv
+    """
+    source_df = pd.read_csv(input_path, low_memory=False)
+    source_df["data_dt"] = pd.to_datetime(source_df.data, format="%d/%m/%Y")
+    source_df[text_columns] = (
+        source_df[text_columns].astype(str).fillna("").replace("nan", "", regex=False)
+    )
+    # keep the latest consultation for each patient
+    df = source_df.sort_values(by=["data_dt"], ascending=False).drop_duplicates(
+        subset=["rghc"]
+    )
+    # concatenate the relevant sections into one single text
+    df["text"] = df[text_columns].apply(lambda row: " ".join(row.values), axis=1)
+    # add a person_id for deidentification
+    df["person_id"] = df.index
+    df[["rghc", "person_id", "data", "data_dt", "text"] + text_columns].to_csv(
+        output_path, index=False
+    )
+    return
+
+
+if __name__ == "__main__":
+    latest_tasy(
+        input_path="src/data/mieloma_olivier_1.csv",
+        output_path="src/data/latest_tasy.csv",
+        text_columns=tasy_tratamento_columns,
+    )
