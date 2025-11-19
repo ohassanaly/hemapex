c7e7cf82abba8278d535322747eadc1df5880739
ohassanaly <olivierhass@gmail.com>
2025-11-19 14:39:13 +0100

    code cleaning and adding Mistral API

--- src/api_calls.py
+++ src/api_calls.py
@@ -15,6 +15,13 @@ class Provider(str, Enum):
     MISTRAL = "mistral"
 
 
+MODELS = {
+    Provider.OPENAI: "gpt-4.1",
+    Provider.GEMINI: "gemini-2.5-pro",
+    Provider.MISTRAL: "open-mistral-7b",  # free tier
+}
+
+
 def call_llm_structured(
     provider: Provider,
     model: str,
@@ -24,8 +31,9 @@ def call_llm_structured(
 ) -> T:
     if provider == Provider.OPENAI:
         from openai import OpenAI
+        import os
 
-        client = OpenAI()
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
         response = client.responses.parse(
             model=model,
             instructions=instruction,
@@ -37,8 +45,9 @@ def call_llm_structured(
 
     elif provider == Provider.GEMINI:
         from google import genai
+        import os
 
-        client = genai.Client()
+        client = genai.Client(os.getenv("GEMINI_API_KEY"))
         response = client.models.generate_content(
             model=model,
             contents=("\n").join([instruction, unstructured_text]),
@@ -49,21 +58,47 @@ def call_llm_structured(
         )
         return schema_model.model_validate_json(response.text)
 
+    elif provider == Provider.MISTRAL:
+        from mistralai import Mistral
+
+        import os
+        import json
+
+        client = Mistral(api_key=os.getenv("MISTRAL_API_KEY"))
+
+        system_msg = ("\n").join(
+            [
+                instruction,
+                "You MUST produce JSON that matches this schema exactly :",
+                json.dumps(schema_model.model_json_schema()),
+            ]
+        )
+
+        response = client.chat.complete(
+            model=model,
+            messages=[
+                {"role": "system", "content": system_msg},
+                {"role": "user", "content": unstructured_text},
+            ],
+            response_format={
+                "type": "json_object",
+            },
+        )
+        return schema_model.model_validate_json(response.choices[0].message.content)
+
     else:
         raise ValueError(f"Unknown provider: {provider}")
 
 
 if __name__ == "__main__":
-    from dotenv import load_dotenv
     from structured_treatment import TreatmentLines
     from datetime import datetime
     from pathlib import Path
+    from dotenv import load_dotenv
     import json
 
-    # provider = Provider.OPENAI
-    # model = "gpt-4.1"
-    provider = Provider.GEMINI
-    model = "gemini-2.5-pro"
+    provider = Provider.OPENAI
+    model = MODELS[provider]
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     output_path = (
@@ -71,6 +106,7 @@ if __name__ == "__main__":
     )
 
     load_dotenv()
+
     result = call_llm_structured(
         provider=provider,
         model=model,
